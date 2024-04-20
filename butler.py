6587138c1b0109b2b3d622f0241d1e67fccf8db7
sTATic-WalKing <487666740@qq.com>
2024-04-20 19:20:32 +0800

    init

---
+++ butler.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8-*-
+from robot.sdk import unit
+from robot.sdk.AbstractPlugin import AbstractPlugin
+import requests
+import json
+from robot.Player import SoxPlayer
+from recognizers_date_time import recognize_datetime, Culture
+import datetime
+
+def get_name(): 
+    return '智能音箱'
+
+def get_view_404_string():
+    return '好像找不到这个任务。'
+
+def get_furniture_404_string():
+    return '好像找不到这个家具。'
+
+def get_state_failed_string():
+    return '这个家具好像出错了。'
+
+def get_state_string(type, state):
+    strings = [
+        ['关闭', '打开']
+    ]
+    return strings[type][state]
+
+def get_state_furniture_string(furniture, state):
+    ret = get_state_string(furniture['type'], state)
+    if 'loc' in furniture:
+        ret += furniture['loc'] + '的'
+    if 'alias' in furniture:
+        ret += furniture['alias']
+    else:
+        ret += '家具'
+    return ret
+
+def get_date_string(dt):
+    now = datetime.datetime.now()
+    if dt.year == now.year:
+        if dt.month == now.month:
+            offset = dt.day - now.day
+            if offset == 0:
+                return ''
+            return dt.strftime('%d日')
+        return dt.strftime('%m月%d日')
+    return dt.strftime('%Y年%m月%d日')
+
+def get_time_string(dt):
+    if dt.second == 0:
+        return dt.strftime('%H时%M分')
+    return dt.strftime('%H时%M分%S秒')
+
+def get_auto_string(dt, view):
+    return '将在' + get_date_string(dt) + get_time_string(dt) + '执行任务' + view['alias'] + '。'
+
+def get_furniture_string(furniture, state):
+    return '已' + get_state_furniture_string(furniture, state)
+
+def get_furniture2auto_string(dt, furniture, state):
+    return '将在' + get_date_string(dt) + get_time_string(dt) + get_state_furniture_string(furniture, state)
+
+def postJSON(method, input=None):
+    response = requests.post('http://localhost:11151' + method, data=json.dumps(input))
+    if response.status_code != 200:
+        raise Exception(response)
+    output = json.loads(response.content)
+    return output
+
+def hit(text, marks):
+    for mark in marks:
+        if text.find(mark) != -1:
+            return True
+    return False
+
+def hit_auto(text):
+    marks = ['执行', '任务']
+    return hit(text, marks)
+
+def get_expect_state(text):
+    marks = ['关']
+    if hit(text, marks):
+        return 0
+    else:
+        return 1
+
+def get_datetime(text):
+    results = recognize_datetime(text, Culture.Chinese)
+    last = len(results) - 1
+    if last >= 0:
+        value = results[last].resolution['values'][0]
+        print(value)
+        if value['type'] == 'date':
+            date = datetime.date.fromisoformat(value['value'])
+            now = datetime.datetime.now()
+            time = datetime.time(hour=now.hour,minute=now.minute,second=now.second)
+            return int(datetime.datetime.combine(date, time).timestamp())
+        elif value['type'] == 'time':
+            time = datetime.time.fromisoformat(value['value'])
+            return int(datetime.datetime.combine(datetime.date.today(), time).timestamp())
+        elif value['type'] == 'datetime':
+            return int(datetime.datetime.strptime(value['value'], '%Y-%m-%d %H:%M:%S').timestamp())
+        elif value['type'] == 'datetimerange':
+            start = int(datetime.datetime.strptime(value['start'], '%Y-%m-%d %H:%M:%S').timestamp())
+            end = int(datetime.datetime.strptime(value['end'], '%Y-%m-%d %H:%M:%S').timestamp())
+            return (start + end) // 2
+    return 0
+
+def calculate_configs(text):
+    input = {}
+    input['connected'] = True
+    addresses = postJSON('/filter', input)['addresses']
+    configs = []
+    for address in addresses:
+        input = {}
+        input['address'] = address
+        configs.append(postJSON('/config', input))
+    for config in configs:
+        config['score'] = 0
+        if 'alias' in config:
+            configAlias = config['alias']
+            if text.find(configAlias) != -1:
+                config['score'] += len(configAlias)
+        if 'loc' in config:
+            configLoc = config['loc']
+            if text.find(configLoc) != -1:
+                config['score'] += len(configLoc)
+    return configs
+
+def calculate_views(text):
+    uids = postJSON('/views', {})['uids']
+    views = []
+    for uid in uids:
+        input = {}
+        input['uid'] = uid
+        views.append(postJSON('/view', input))
+    for view in views:
+        view['score'] = 0
+        if 'alias' in view:
+            viewAlias = view['alias']
+            if text.find(viewAlias) != -1:
+                view['score'] += len(viewAlias)
+    return views
+
+class Plugin(AbstractPlugin):
+
+    def __init__(self, con):
+        super(Plugin, self).__init__(con)
+        self.player = SoxPlayer()
+
+    def isValid(self, text, parsed):
+        return True
+
+    def handle(self, text, parsed):
+        if hit_auto(text):
+            self.handle_auto(text, parsed)
+        else:
+            self.handle_furniture(text, parsed)
+
+    def handle_furniture(self, text, parsed):
+        configs = calculate_configs(text)
+        configs.sort(key=lambda a: a['score'], reverse=True)
+        if len(configs) > 0 and configs[0]['score'] > 0:
+            start = get_datetime(text)
+            if start == 0:
+                input = {}
+                input['address'] = configs[0]['address']
+                expect = get_expect_state(text)
+                input['state'] = expect
+                output = postJSON('/state', input)
+                if output['state'] == expect:
+                    self.say(get_furniture_string(configs[0], expect))
+                else:
+                    self.say(get_state_failed_string())
+            else:
+                state = {}
+                state['address'] = configs[0]['address']
+                expect = get_expect_state(text)
+                state['state'] = expect
+                states = []
+                states.append(state)
+                input = {}
+                input['states'] = states
+                input['alias'] = get_name()
+                output = postJSON('/view', input)
+                input = {}
+                input['view'] = output['uid']
+                input['start'] = start
+                postJSON('/auto', input)
+                self.say(get_furniture2auto_string(datetime.datetime.fromtimestamp(start), configs[0], expect))
+        else:
+            self.say(get_furniture_404_string())
+    
+    def handle_auto(self, text, parsed):
+        views = calculate_views(text)
+        views.sort(key=lambda a: a['score'], reverse=True)
+        if len(views) > 0 and views[0]['score'] > 0:
+            start = get_datetime(text)
+            if start == 0:
+                start = int(datetime.datetime.now().timestamp())
+            input = {}
+            input['start'] = start
+            input['view'] = views[0]['uid']
+            postJSON('/auto', input)
+            self.say(get_auto_string(datetime.datetime.fromtimestamp(start)), views[0])
+        else:
+            self.say(get_view_404_string())
+
+    
+        
+        
+
+        
